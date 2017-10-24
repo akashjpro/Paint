d88739d6d043e8ff8abf1b24f73cf7a5018017f2
ISB-VIETNAM\tmha <tmha@vn.isb.co.jp>
2017-10-24 17:25:47 +0700

    Paint

---
+++ app/src/main/java/com/example/tmha/paint/FingerPath.java
@@ -0,0 +1,23 @@
+package com.example.tmha.paint;
+
+import android.graphics.Path;
+
+/**
+ * Created by tmha on 10/24/2017.
+ */
+
+public class FingerPath {
+    public int color;
+    public boolean emboss;
+    public boolean blur;
+    public int strokeWidth;
+    public Path path;
+
+    public FingerPath(int color, boolean emboss, boolean blur, int strokeWidth, Path path) {
+        this.color = color;
+        this.emboss = emboss;
+        this.blur = blur;
+        this.strokeWidth = strokeWidth;
+        this.path = path;
+    }
+}
